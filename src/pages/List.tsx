3e6a2ef7a9aa077ea2ca0befc1aa1d3ea904fd5d
Bayathy <misosoup0321@icloud.com>
2022-09-04 17:01:53 +0900

    .

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,10 +1,8 @@
 import { NextPage } from 'next'
-import { CardList } from '../..//components/organisms/CardList'
-import { ListLayout } from '../../components/template/ListLayout'
+import { CardList } from '../components/organisms/CardList'
+import { ListLayout } from '../components/template/ListLayout'
 
 const List: NextPage = () => {
-
-
     return (
         <ListLayout Headertitle="GourMap" isHomeLayout={false}>
             <CardList />
